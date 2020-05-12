85785b5fb7fdafb799a3be6b547c38792d0d2296
kapke07 <kapke72@gmail.com>
2020-05-13 02:47:00 +0300

    Full staff

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+
+particlesJS.load('particles-js', 'particles.json');
+
+
+/* Otherwise just put the config content (json): */
